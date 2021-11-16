0284b0a52d462f1395f37e4e5067c27ba742ddf8
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2021-11-15 20:28:15 -0400

    update to code to make a 2 row and a slightly different fill pallet

--- code/network_scope_run_example.R
+++ code/network_scope_run_example.R
@@ -21,7 +21,7 @@ source("code/network_scope.R")
 bathy <- raster("data/gebco_nw_atlantic.tif")
 
 #load the polygons of the network and convert it to the projection of the bathymetry data we will use
-network <- read_sf("data/shapefiles/networksites_proposed_OEM_MPA_v2.shp")
+network <- read_sf("data/shapefiles/networksites_proposed_OEM_MPA_v2.shp")%>%mutate(network="Draft Network")
 
 #some sites have two seperate polygons entered as individual identities. This will merge them and then bring back in the other columns using left_join() - otherwise lost in group_by
 network <- network%>%
@@ -31,8 +31,9 @@ network <- network%>%
               data.frame()%>%
               distinct(NAME,.keep_all=TRUE)%>%
               mutate(area=as.numeric(st_area(geometry)/1000/1000))%>% #recalculate area for the split areas
-              dplyr::select(NAME,STATUS,area,TYPE))%>%
-  dplyr::select(NAME,STATUS,TYPE,area,geometry)
+              dplyr::select(NAME,STATUS,area,network,TYPE))%>%
+  mutate(species=NA)%>% #this is a dummy variable for ggplot long-form data assembly below
+  dplyr::select(NAME,STATUS,TYPE,network,area,species,geometry)
 
 #create a basemap for plotting (this code can be used each time you make a plot)
 basemap <- rbind(ne_states(country = "Canada",returnclass = "sf")%>%
@@ -76,40 +77,47 @@ species_niche <- read.csv("data/species_niche_final.csv")
 ##now lets view the output
   
   wolffish_network <- st_read("output/species_networks/Anarhichas_lupus_network_trim.shp")%>%
-                      mutate(species="Atlantic wolffish")%>%
-                      st_transform(latlong)
+                      mutate(species="Atlantic wolffish",
+                             network="Niche-adjusted Network")%>%
+                      st_transform(latlong)%>%
+                      dplyr::select(NAME,STATUS,TYPE,network,area,species,geometry)
     
   wolffish_points <- read.csv("output/robis_extractions/Anarhichas_lupus_obis_overlaps.csv")%>%
                      filter(!is.na(X),!is.na(Y))%>%
-                     st_as_sf(coords=c("X","Y"),crs=latlong)%>% #convers the xy data output from the function to a 'sf' object
+                     st_as_sf(coords=c("X","Y"),crs=latlong)%>% #converts the xy data output from the function to a 'sf' object
                     mutate(species="Atlantic wolffish")
   
   winter_network <- st_read("output/species_networks/Leucoraja_ocellata_network_trim.shp")%>%
-                    mutate(species="Winter skate")%>%
-                    st_transform(latlong)
+                    mutate(species="Winter skate",
+                           network="Niche-adjusted Network")%>%
+                    st_transform(latlong)%>%
+                    dplyr::select(NAME,STATUS,TYPE,network,area,species,geometry)
   
   winter_points <- read.csv("output/robis_extractions/Leucoraja_ocellata_obis_overlaps.csv")%>%
                   filter(!is.na(X),!is.na(Y))%>%
-                  st_as_sf(coords=c("X","Y"),crs=latlong)%>% #convers the xy data output from the function to a 'sf' object
+                  st_as_sf(coords=c("X","Y"),crs=latlong)%>% #converts the xy data output from the function to a 'sf' object
                   mutate(species="Winter skate")
   
   
-  networks <- rbind(wolffish_network,winter_network)%>%mutate(legend="Niche adjusted network") #this is a dummy variable so that ggplot will include a mention on the plot
+  networks <- rbind(wolffish_network,
+                    winter_network,
+                    network%>%st_transform(latlong)%>%mutate(species="Atlantic wolffish"),
+                    network%>%st_transform(latlong)%>%mutate(species="Winter skate")) #this is a dummy variable so that ggplot will include a mention on the plot
+  
   obis_points <- rbind(wolffish_points,winter_points)
   
-  
+  #assemble the plot
   p1 <- ggplot()+
-    geom_sf(data=basemap)+ #this is the land
-    geom_sf(data=network,fill="cornflowerblue")+ #plot the original network
-    geom_sf(data=networks,aes(fill=legend))+ #add the buffered polygons on the original network
+    geom_sf(data=basemap,fill="darkolivegreen3")+ #this is the land
+    #geom_sf(data=network,aes(fill=network))+ #plot the original network
+    geom_sf(data=networks,aes(fill=network))+ #add the buffered polygons on the original network
     geom_sf(data=obis_points,size=0.25)+ #add the points on top of the shapes
     coord_sf(expand=0)+# this just gets rid of a plotting buffer that ggplot defaults to
-    #scale_fill_manual(values="coral")+
-    theme_bw()+
+        theme_bw()+
     theme(legend.position="bottom",
           strip.background = element_rect(fill="white"))+
-    facet_wrap(~species,ncol=2)+
-    labs(fill="")
-    
+    facet_wrap(~species,nrow=2)+
+    labs(fill="")+
+    scale_fill_manual(values=c("grey85","coral2"))
   
-  ggsave("inst/comparison_trim_plot.jpg",p1,height=6,width=8,units="in",dpi=300)
+  ggsave("inst/comparison_trim_plot.jpg",p1,height=8,width=5,units="in",dpi=300)
