2004c70df5554752f13f95095875f09e1585c654
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2021-12-02 15:53:16 -0400

    revised to create an example (placeholder) Figure 1

--- code/resolution_check.R
+++ code/resolution_check.R
@@ -4,21 +4,57 @@
 library(sf)
 library(ggplot2)
 library(dplyr)
+library(rnaturalearth)
+
+sf_use_s2(FALSE)
 
 #Projections ------------
 latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
 #load polygons -------------
 bioregion <- read_sf("data/Shapefiles/MaritimesPlanningArea.shp")%>%st_transform(latlong)
-draft_network <- read_sf("data/Shapefiles/networksites_proposed_OEM_MPA_v2.shp")%>%
+
+network_initial <- read_sf("data/shapefiles/networksites_proposed_OEM_MPA_v2.shp")%>%
   st_transform(latlong)%>%
+  filter(NAME != "Bras d’Or Lakes EBSA") # given this is a very esturarine system, I am not sure we want to include for things maybe other than lobster
+
+#some sites have two separate polygons entered as individual identities. This will merge them and then bring back in the other columns using left_join() - otherwise lost in group_by
+network <- network_initial%>%
+  group_by(NAME)%>%
+  summarise(geometry=st_combine(geometry)%>%st_make_valid(),
+            area=as.numeric(st_area(geometry)/1000/1000))%>%
+  ungroup()%>%
+  st_make_valid()%>% #why sf now requires this so much is beyond me
+  left_join(.,network_initial%>%
+              data.frame()%>%
+              dplyr::select(-geometry)%>%
+              distinct(NAME,.keep_all=TRUE))%>%
+  mutate(species=NA,network="Draft Network")%>% #this is for later plotting
+  dplyr::select(NAME,STATUS,TYPE,network,species,geometry)
+
+draft_network <- network%>%
   mutate(area=round((st_area(geometry)/1000/1000),2)%>%as.numeric()) #area in km2 converted from m2
 
 #create a grid at 0.25 degrees which fits the downscale, scale that Dan Boyce set up. This is for visualization only
 study_grid <- bioregion%>%st_make_grid(cellsize=0.25)
 bioregion_grid <- bioregion%>%st_intersection(study_grid)
 sites_grid <- draft_network%>%
+              st_make_valid()%>%
               st_intersection(study_grid)
+
+#create a basemap for plotting (this code can be used each time you make a plot)
+basemap <- rbind(ne_states(country = "Canada",returnclass = "sf")%>%
+                   dplyr::select(latitude,longitude,geonunit,geometry)%>%
+                   st_union()%>% #group provinces + territories
+                   st_as_sf()%>%
+                   st_transform(latlong),
+                 ne_states(country = "United States of America",returnclass = "sf")%>%
+                   dplyr::select(latitude,longitude,geonunit,geometry)%>%
+                   st_union()%>% #group provinces + territories
+                   st_as_sf()%>%
+                   st_transform(latlong))%>%
+  st_intersection(.,bioregion%>%st_buffer(0.15)%>%st_make_valid()%>%st_transform(latlong)%>%st_bbox()%>%st_as_sfc()%>%st_as_sf())# this will trim the polygon to the extent of our focal area of interest using a bounding box
+
 
 #Show it in a plot
 p1 <- ggplot()+
@@ -28,6 +64,21 @@ p1 <- ggplot()+
   theme_bw()
 
 ggsave("inst/network_grid.png",p1,width=6,height=6,dpi=600,units="in") #this is used by the readme
+
+#figure 1 example 
+p2 <- ggplot()+
+  geom_sf(data=basemap,fill="darkolivegreen3")+
+  geom_sf(data=bioregion,fill=NA)+
+  geom_sf(data=sites_grid,fill=NA,lwd=0.25)+
+  geom_sf(data=draft_network,fill=NA,lwd=0.8)+
+  theme_bw()+
+  theme(panel.grid=element_blank())+
+  coord_sf(expand=0)
+
+ggsave("output/Fig1_ex.png",p2,width=6,height=6,dpi=600,units="in")
+
+
+
 
 #count the number of grid cells occupied by each site
 grid_count <- sites_grid%>%
