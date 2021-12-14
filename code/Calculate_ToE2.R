323e6b059860402a46b7d4934e27f68128ed43e2
Stortini <Christine.Stortini@dfo-mpo.gc.ca>
2021-12-13 22:05:41 -0400

    Code to extract climate projections to species-specific cropped networks for each model is complete. Extractions are saved in "climate_extracts" folder. Code to generate time series of max annual temperature (from monthly means) for each cell within the sites, for each model is also complete. Time series are saves in "maxt_timeseries" folder. Now writing code to get ToE for each cell, averaged across models (for each RCP). Will then merge with network shapefile to get site names.

--- code/Calculate_ToE2.R
+++ code/Calculate_ToE2.R
@@ -18,9 +18,8 @@ latlong <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
 ### STEP 1: Create a time series of maximum monthly average per year for each model/emissions scenario ###
 #create a file path to your climate projection files. Keep in the folders (2.6,8.5) Dan had them in
-fls<-c(list.files("data/climate_projections/2.6/", full.names=T),list.files("data/climate_projections/8.5/",full.names=T)) ##climate projections for RCP 2.6; run again for the 4.5 folder
+fls<-c(list.files("data/climate_projections/2.6/", full.names=T),list.files("data/climate_projections/8.5/",full.names=T)) ##climate projections for RCP 2.6; run again for the 8.5 folder
 l<-list()
-l2<-list()
 
 #List of speices-niche constratined networks - this is the output of species_niches
 network<-list.files('output/species_networks/', pattern="*.shp", full.names=T, recursive=FALSE)#put all cropped networks here with species name as the first part of the file name
@@ -73,13 +72,13 @@ network<-list.files('output/species_networks/', pattern="*.shp", full.names=T, r
         
         }
         
-        save(out_masks,file="data/raster_masks.RData") #save this a list object that can be pulled in sequentially in the next bit of cod
+        save(out_masks,file="data/raster_masks.RData") #save this as list object that can be pulled in sequentially in the next bit of code
         save(out_networks,file="data/network_sp.RData") #conversion of these complex adjusted sites to SP takes time so why do it twice?
         
       }else{load("data/raster_masks.RData")
             load("data/network_sp.Rdata")}
 
-#Now do the data extractions ----------
+#Now do the data extractions ---------- do once for RCP2.6, then again for fls=RCP8.5
     for(i in 1:length(fls)){
         
         #get information on the projection we are working with ----
@@ -159,12 +158,71 @@ network<-list.files('output/species_networks/', pattern="*.shp", full.names=T, r
           save(cmip_extracts,file=paste0("output/climate_extracts/climate_extracts_",mod,"_",climate_proj,".RData"))
           
           #if you want to do csvs but they will be enormous
-          # cmip_out <- do.call("rbind",cmip_extracts)%>%
-          #             data.frame()
-          # 
-          # #write the proposal
-          # write.csv(cmip_out,file=paste0("output/climate_extracts/climate_extracts_",mod,"_",climate_proj,".csv")) 
+          #cmip_out <- do.call("rbind",cmip_extracts)%>%data.frame()
+          #
+          #write.csv(cmip_out,file=paste0("output/climate_extracts/climate_extracts_",mod,"_",climate_proj,".csv")) 
              
     } #end loop through the projections
         
-   
+
+### Read in cmip_extracts and reduce to one value (max temp) per year for each species/site/cell/model/emissions scenario
+ces<-c(list.files("output/climate_extracts/", full.names=T, pattern="*.RData", recursive=F))
+l<-list()
+mmfun<-function(d){ #this is a function to calculate max temp
+  max<-data.frame(maxT=max(d$value,na.rm=TRUE))
+  return(max)
+}
+require(plyr)
+for(i in 1:length(ces)){
+  load(ces[i])
+  data=do.call("rbind",cmip_extracts)
+  data$site="site name here"
+  dvel<-ddply(data,.(x,y,species,mod,climate_proj,year),.fun=mmfun,.progress='text')
+  l[[i]]<-dvel}
+require(data.table)
+max<-data.frame(rbindlist(l))
+
+## Now merge with species list to get upper thermal limit for each species
+spp<-read.csv("data/species_niche_final.csv")
+names(spp)
+spp$species=spp$SciName
+spp<-spp[,c(4,9)]
+df<-merge(max,spp,by="species")
+
+##############################################################
+findToE<-function(maxT,ProjTS,yearsProj,runLength=2){
+  
+  ##IF SPECIES DOES NOT EMERGE FROM ITS NICHE BY END OF PROJECTION, SET TO THIS
+  ToEFirst<-max(yearsProj)+1
+  
+  emYears<-which(ProjTS>maxT)
+  
+  if(length(emYears)>0){
+    
+    x<-rep(0,length(yearsProj)) # create binary vector showing emergent (1) or non-emergent (0) years
+    x[emYears]<-1
+    
+    #calculate the year when the temperature emerges for a period of at least 'runLength' years
+    diffs <- x[-1L] != x[-length(x)] # find where the values change
+    idx <- c(which(diffs), length(x)) # get the indexes, and then get the difference in subsequent indexes
+    runs<-diff(c(0, idx))# calculate the length of the runs
+    x2<-x[-which(c(NA,diff(x))==0)] # determine whether each run is a run of 1's or 0's
+    yearsRunStart<-yearsProj[-which(c(NA,diff(x))==0)] # find the start year of each run
+    runsTab<-data.frame(year=yearsRunStart,val=x2,run=runs) # store all this in a table
+    runsTab<-runsTab[runsTab$val==1,] # remove runs of 0's
+    runStart<-which(runsTab$run>=runLength)
+    if(length(runStart)>0){
+      ToEFirst<-min(runsTab$year[runStart]) #find the first year where there is a run of 1's equal to or greater than runLength
+    }
+    
+  }
+  return(ToEFirst)
+}
+
+
+
+network <- read_sf("data/shapefiles/networksites_proposed_OEM_MPA_v2.shp")%>%mutate(network="Draft Network")
+
+
+
+
